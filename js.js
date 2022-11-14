c8c9827991491cb47a89dd4751492eaf7fc51e58
Michal Matuszewski <michal.matuszewski1984@gmail.com>
2022-11-14 16:14:11 +0100

    changed formula for greatest increase/decrease profits

--- js.js
+++ js.js
@@ -7,26 +7,39 @@
 let total = 0;
 let average_change = 0;
 var avr_cha = 0;
+var gre = 0;
+var gre_date = 0;
+var decrease = 0;
+var decrease_date = 0;
+var j = 1;
 
 // creating loop to count total sum of average income placed in second position of array,
 // then reverse it so I can use sort algoritm by number not by dates.
 
 for (let i = 0; i < finances.length; i++) {
     total += finances[i][1];
+    j = i + 1;
 
-    for (let j = 1; j < finances.length; j++) {
+    if (j < finances.length) {
 
-        // ***average*** change ver 2 new different formula
-        // How do I calculate Month-over-Month growth?
-        // Percent increase (or decrease) = (Month 2 - Month 1) / Month 1 * 100
-        // Or like this if you prefer a more minimalist approach:
-        // x = (y - z) / z * 100
+        if (((finances[j][1]) - (finances[i][1])) > gre) {
+            gre = ((finances[j][1]) - (finances[i][1]));
+            gre_date = finances[j][0];
+        }
 
-        avr_cha += (finances[j][1] - finances[i][1]) / (finances[i][1] * 100);
-
+        else if (((finances[j][1]) - (finances[i][1])) < decrease) {
+            decrease = ((finances[j][1]) - (finances[i][1]));
+            decrease_date = finances[j][0];
+        }
     }
-
 }
+
+
+
+
+
+
+
 
 // rounded to 2 places after decimal
 var avr_cha_rounded = parseFloat(avr_cha).toFixed(2);
@@ -41,7 +54,6 @@ console.log(`Financial Analysis\n------------------------`);
 console.log(`Total Months: ${finances.length}`);
 console.log(`Total: $${total}`);
 console.log(`Average Change: $ ${Math.round(total / finances.length)}`);
-console.log(`Average Change New Formula: ${avr_cha_rounded}%`);
-console.log(`Greatest Increase in Profits: ${finances[finances.length - 1][0]} ($ ${finances[finances.length - 1][1]})`);
-console.log(`Greatest Decrease in Profits: ${finances[0][0]} ($ ${finances[0][1]})`);
+console.log(`Greatest Increase in Profits: ${gre_date} ($ ${gre})`);
+console.log(`Greatest Decrease in Profits: ${decrease_date} ($ ${decrease})`);
 
